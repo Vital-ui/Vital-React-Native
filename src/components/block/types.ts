557735d2e966b6a0d5bfe359a4f3c2dad997d8e6
satyamyadav20 <35507071+satyamyadav20@users.noreply.github.com>
2025-07-04 17:59:40 +0530

    block component refactored

--- src/components/block/types.ts
+++ src/components/block/types.ts
@@ -1,37 +1,38 @@
-import type React from "react";
+import type {ReactNode} from "react";
 import type {Falsy, RecursiveArray, RegisteredStyle, TextStyle, ViewStyle} from "react-native";
 
 export type TitleType = {
-    titleLeft?: React.ReactChild | React.ReactFragment;
+    titleLeft?: ReactNode;
     blockTitleSize?: TextStyle | RegisteredStyle<TextStyle> | RecursiveArray<TextStyle | Falsy | RegisteredStyle<TextStyle>> | readonly (TextStyle | Falsy | RegisteredStyle<TextStyle>)[];
     blockTitleStyle?: TextStyle | RegisteredStyle<TextStyle> | RecursiveArray<TextStyle | Falsy | RegisteredStyle<TextStyle>> | readonly (TextStyle | Falsy | RegisteredStyle<TextStyle>)[];
-    title?: React.ReactChild | React.ReactFragment;
+    title?: ReactNode
+    fontConfig?: TextStyle | RegisteredStyle<TextStyle>;
 }
 
 export type BlockType = TitleType & {
     titleMargin?: {
-        margin?: string | number;
-        marginHorizontal?: string | number;
-        marginVertical?: string | number;
-        marginTop?: string | number;
-        marginBottom?: string | number;
-        marginRight?: string | number;
-        marginLeft?: string | number;
+        margin?: number;
+        marginHorizontal?: number;
+        marginVertical?: number;
+        marginTop?: number;
+        marginBottom?: number;
+        marginRight?: number;
+        marginLeft?: number;
     };
     blockMargin?: {
-        margin?: string | number;
-        marginHorizontal?: string | number;
-        marginVertical?: string | number;
-        marginTop?: string | number;
-        marginBottom?: string | number;
-        marginRight?: string | number;
-        marginLeft?: string | number;
+        margin?: number;
+        marginHorizontal?: number;
+        marginVertical?: number;
+        marginTop?: number;
+        marginBottom?: number;
+        marginRight?: number;
+        marginLeft?: number;
     };
     titleFluid?: boolean;
     header?: boolean;
     contentContainerStyle?: ViewStyle | RegisteredStyle<ViewStyle> | RecursiveArray<ViewStyle | Falsy | RegisteredStyle<ViewStyle>> | readonly (ViewStyle | Falsy | RegisteredStyle<ViewStyle>)[];
-    children?: React.ReactChild | React.ReactFragment;
+    children?: ReactNode
     style?: ViewStyle | RegisteredStyle<ViewStyle> | RecursiveArray<ViewStyle | Falsy | RegisteredStyle<ViewStyle>> | readonly (ViewStyle | Falsy | RegisteredStyle<ViewStyle>)[];
     fluid?: boolean;
-    blockHeaderRight?: React.ReactChild | React.ReactFragment;
+    blockHeaderRight?: ReactNode
 }
--- src/index.tsx
+++ src/index.tsx
@@ -95,5 +95,5 @@ export {
     ScreenOrientation,
     Insets,
     InAppExperience,
-    NavigationBar,
+    NavigationBar
 };
