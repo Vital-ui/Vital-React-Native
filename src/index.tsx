e36b7eba907f188a583b85fe9e16e6f2752daf9e
satyamyadav20 <35507071+satyamyadav20@users.noreply.github.com>
2025-07-07 16:46:41 +0530

    restructure component folder and separate styles

--- src/index.tsx
+++ src/index.tsx
@@ -27,7 +27,7 @@ import Accordion from "./components/accordion/accordion";
 import GradientImage from "./components/gradientImage/gradientImage";
 import GradientBlock from "./components/gradientBlock/gradientBlock";
 import Block from "./components/block/block";
-import Checkbox from "./components/checkbox/checkbox";
+import Checkbox from "./components/checkbox";
 import RadioButton from "./components/radioButton/radioButton";
 import Button from "./components/button/button";
 import Carousel from "./components/carousel/carousel";
