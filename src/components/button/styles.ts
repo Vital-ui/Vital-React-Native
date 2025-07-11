8736da6f5c0af9bc7ca9543e8250c38707fca8d0
satyamyadav20 <35507071+satyamyadav20@users.noreply.github.com>
2025-07-11 15:47:33 +0530

    organize props structure and extract styles into separate file

--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -1,4 +1,4 @@
-import { Dimensions, StyleSheet } from 'react-native';
+import {Dimensions, StyleSheet} from "react-native";
 const {height: SCREEN_HEIGHT} = Dimensions.get("screen");
 
 export const componentStyles = StyleSheet.create({
