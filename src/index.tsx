0bec4cddf5a895a55670d1f4a7dc76a9f2383d57
Ajay Singh <117065425+ajaysingh266@users.noreply.github.com>
2025-07-18 00:52:21 +0530

    Gradient image (#19)
    * refactor: simplify and optimize GradientImageProps
    
    * file name and import statement minor change
    
    ---------
    
    Co-authored-by: ajaysinghfacillima <147366593+ajaysinghfacillima@users.noreply.github.com>
    Co-authored-by: sgoel1495 <60220964+sgoel1495@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,11 @@ import {height} from "./components/height/height";
 import {width} from "./components/width/width";
 import {fontSize} from "./components/typography/fontSize";
 import {
-    setTypographyConfig,
-    getTypographyConfig,
-    resetTypographyConfig,
     getRootFontSize,
+    getTypographyConfig,
     rem,
+    resetTypographyConfig,
+    setTypographyConfig,
 } from "./components/typography/config";
 import GradientText from "./components/typography/GradientText";
 import H1 from "./components/typography/h1";
@@ -23,8 +23,8 @@ import H7 from "./components/typography/h7";
 import H8 from "./components/typography/h8";
 import H9 from "./components/typography/h9";
 import Container from "./components/container/container";
+import GradientImage from "./components/gradientImage";
 import Accordion from "./components/accordion";
-import GradientImage from "./components/gradientImage/gradientImage";
 import GradientBlock from "./components/gradientBlock/gradientBlock";
 import Block from "./components/block/block";
 import Checkbox from "./components/checkbox/checkbox";
@@ -45,7 +45,8 @@ import Calender from "./components/calender/calender";
 import Slider from "./components/slider/Slider";
 import Header from "./components/header/Header";
 
-const {ScreenOrientation, Insets, InAppExperience, NavigationBar} = NativeModules;
+const {ScreenOrientation, Insets, InAppExperience, NavigationBar} =
+    NativeModules;
 
 export {
     margin,
@@ -94,5 +95,5 @@ export {
     ScreenOrientation,
     Insets,
     InAppExperience,
-    NavigationBar
+    NavigationBar,
 };
