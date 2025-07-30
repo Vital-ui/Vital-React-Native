a39d2c11756a4260937bea1f9c7838430a09fcd9
satyamyadav20 <35507071+satyamyadav20@users.noreply.github.com>
2025-07-30 18:04:57 +0530

    - Update metro.config.js to use react-native-svg-transformer - Install react-native-svg and react-native-svg-transformer

--- example/metro.config.js
+++ example/metro.config.js
@@ -10,8 +10,22 @@ const root = path.resolve(__dirname, "..");
  * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('metro-config').MetroConfig}
+ *
  */
-module.exports = getConfig(getDefaultConfig(__dirname), {
+
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
+defaultConfig.transformer = {
+    ...defaultConfig.transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+};
+defaultConfig.resolver = {
+    ...defaultConfig.resolver,
+    assetExts: assetExts.filter(ext => ext !== "svg"),
+    sourceExts: [...sourceExts, "svg"],
+};
+
+module.exports = getConfig(defaultConfig, {
     root,
     pkg,
     project: __dirname,
