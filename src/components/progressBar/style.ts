4c778cb932ae98520799fd312cd92cd4f01aaef8
Ajay Singh <117065425+ajaysingh266@users.noreply.github.com>
2025-08-05 23:04:47 +0530

    feat: add animated ProgressBar component (#22)
    Co-authored-by: Rahul Bhatt <93994083+thedomguy@users.noreply.github.com>

---
+++ src/components/progressBar/style.ts
@@ -0,0 +1,14 @@
+import {StyleSheet} from "react-native";
+
+export const progressBarStyles = StyleSheet.create({
+    container: {
+        overflow: "hidden",
+        backgroundColor: "#EAEBEE",
+        position: "relative",
+    },
+    gradient: {
+        width: "100%",
+        height: "100%",
+        borderRadius: 999,
+    },
+});
