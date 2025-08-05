7f17229d0dab3e5ab62d5b08a1217c1706706eef
satyamyadav20 <35507071+satyamyadav20@users.noreply.github.com>
2025-08-05 21:38:44 +0530

    test component

--- example/src/Dashboard/index.tsx
+++ example/src/Dashboard/index.tsx
@@ -1,10 +1,65 @@
 import React from 'react';
 import {Text, View} from "react-native";
+import { Accordion, borderRadius, Button, Checkbox, Dialog, padding } from 'vital-react-native';
 
 const Index = () => {
+    const [checked, setChecked] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
     return (
-        <View>
-            <Text>Dashboard</Text>
+        <View style={[padding.p5, {flex: 1, gap: 10}]}>
+            {/*<Text>Dashboard</Text>*/}
+            <Accordion
+                title={<Text>Dashboard Component</Text>}
+                showIcon
+                // open={true}
+                // styles={{
+                //     style: undefined,
+                //     titleStyle: undefined,
+                //     bodyStyle: undefined,
+                // }}
+            >
+                <Text>Hello World</Text>
+            </Accordion>
+            <Checkbox
+                size={20}
+                borderColor={"#000000"}
+                backgroundColor={"#BCBCBC"}
+                fill={true}
+                checkColor={"red"}
+                checked={checked}
+                onPress={() => {setChecked(!checked)}}
+            >
+                <View>
+                    <Text>
+                        CheckBox
+                    </Text>
+                </View>
+            </Checkbox>
+            <Dialog
+                visible={open}
+                onClose={() => {}}
+                // style={}
+                onBackDropPress={() => setOpen(false)}
+                header={true}
+                headerColor={"red"}
+                title={(<Text>Hello World</Text>)}
+                onRequestClose={() => setOpen(false)}
+                bodyColor={"white"}
+                onRequestOpen={() => setOpen(true)}
+            >
+                <View>
+                    <Text>Ram Ram</Text>
+                </View>
+            </Dialog>
+            <Button
+                borderRadius={borderRadius.br5}
+                padding={padding.p4}
+                bordered={true}
+                backgroundColor={"#FFFFFF"}
+                onPress={() => {setOpen(!open)}}
+            >
+                <Text>Dialog</Text>
+            </Button>
         </View>
     );
 };
