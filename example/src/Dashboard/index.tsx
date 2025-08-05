718d26859172c70822ec7ec8b8c8eacc3be8854f
satyamyadav20 <35507071+satyamyadav20@users.noreply.github.com>
2025-08-05 23:32:54 +0530

    share Tabler Custom #1

--- example/src/Dashboard/index.tsx
+++ example/src/Dashboard/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Text, View} from "react-native";
-import { Accordion, borderRadius, Button, Checkbox, Dialog, padding } from 'vital-react-native';
+import { Text, View } from 'react-native';
+import { Accordion, borderRadius, Button, Checkbox, Dialog, padding, Switch } from 'vital-react-native';
 
 const Index = () => {
     const [checked, setChecked] = React.useState(false);
     const [open, setOpen] = React.useState(false);
     return (
-        <View style={[padding.p5, {flex: 1, gap: 10}]}>
+        <View style={[padding.p5, { flex: 1, gap: 10 }]}>
             {/*<Text>Dashboard</Text>*/}
             <Accordion
                 title={<Text>Dashboard Component</Text>}
@@ -27,12 +27,12 @@ const Index = () => {
                 fill={true}
                 checkColor={"red"}
                 checked={checked}
-                onPress={() => {setChecked(!checked)}}
+                onPress={() => {
+                    setChecked(!checked);
+                }}
             >
                 <View>
-                    <Text>
-                        CheckBox
-                    </Text>
+                    <Text>CheckBox</Text>
                 </View>
             </Checkbox>
             <Dialog
@@ -42,7 +42,7 @@ const Index = () => {
                 onBackDropPress={() => setOpen(false)}
                 header={true}
                 headerColor={"red"}
-                title={(<Text>Hello World</Text>)}
+                title={<Text>Hello World</Text>}
                 onRequestClose={() => setOpen(false)}
                 bodyColor={"white"}
                 onRequestOpen={() => setOpen(true)}
@@ -56,10 +56,24 @@ const Index = () => {
                 padding={padding.p4}
                 bordered={true}
                 backgroundColor={"#FFFFFF"}
-                onPress={() => {setOpen(!open)}}
+                onPress={() => {
+                    setOpen(!open);
+                }}
             >
                 <Text>Dialog</Text>
             </Button>
+
+            <Switch
+                activeTrackColors={["#6AA5FF", "#E02BF0"]}
+                // start={}
+                // end={}
+                // thumbStyle={}
+                defaultValue={true}
+                value={false}
+                // onChange={() => {}}
+                // isDark={true}
+                // disabled={true}
+            />
         </View>
     );
 };
