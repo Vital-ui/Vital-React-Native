a6fc24ce5f01f01cfc9f857329a39560819ecb46
satyamyadav20 <35507071+satyamyadav20@users.noreply.github.com>
2025-08-11 19:13:17 +0530

    block component refactored. updated styles

--- src/components/block/index.tsx
+++ src/components/block/index.tsx
@@ -1,37 +1,46 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 import React from "react";
-import {View} from "react-native";
+import {StyleSheet, TextStyle, View, ViewStyle} from "react-native";
 import {margin} from "../spacing/margin";
 import type {BlockType} from "./types";
 import Title from "./title";
 import styles from "./styles";
 
+export const pickMarginStyles = (style: any) => {
+    const hasAnyMargin = Object.keys(style).some((key) => key.startsWith("margin"));
+    const styleWithMargin = hasAnyMargin ? style : {...style, ...margin.mb2};
+    const marginEntries = Object.fromEntries(Object.entries(styleWithMargin).filter(([key]) => key.startsWith("margin")));
+    const nonMarginEntries = Object.fromEntries(Object.entries(styleWithMargin).filter(([key]) => !key.startsWith("margin")));
+    return {
+        marginEntries,
+        nonMarginEntries,
+    };
+};
+
 function Block(props: BlockType) {
-    const marginSet = props.titleMargin ?? margin.mb2;
-    const blockMargin = props.blockMargin ?? margin.mb5;
+    const style = StyleSheet.flatten(props.style) as ViewStyle;
+    const contentContainerStyle = StyleSheet.flatten(props.contentContainerStyle) as ViewStyle & TextStyle;
+    const titleStyle = StyleSheet.flatten(props.titleStyle) as TextStyle & ViewStyle;
+
     return (
-        <View style={[blockMargin, props.style]}>
+        <View style={style}>
             <View
                 style={
                     props.titleFluid ? styles.containerFluid : styles.container
                 }
             >
-                {props.header ? (
-                    <View style={[marginSet, styles.headerStyle]}>
+                {props.title && (
+                    <View style={[styles.headerStyle, pickMarginStyles(titleStyle).marginEntries]}>
                         <Title {...props} />
-                        {props.blockHeaderRight ?? null}
+                        {props.titleRight ?? null}
                     </View>
-                ) : (
-                    props.title && (
-                        <Title {...props} blockTitleStyle={marginSet} />
-                    )
                 )}
             </View>
             <View
                 style={[
                     props.fluid ? styles.containerFluid : styles.container,
-                    props.contentContainerStyle,
+                    contentContainerStyle,
                 ]}
             >
                 {props.children}
